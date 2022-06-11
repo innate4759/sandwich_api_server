75585f58a46ad60665e5518822d721fe79267987
박현규 <piga2000@builton.co.kr>
2022-06-11 23:27:27 +0900

    # 샌드위치, 빵, 토핑, 치즈, 소스 모델 추가 & migrate

--- sandwich_server/api/models.py
+++ sandwich_server/api/models.py
@@ -1,3 +1,86 @@
 from django.db import models
 
-# Create your models here.
+
+class Bread(models.Model):
+    id = models.AutoField(primary_key=True)
+    is_deleted = models.BooleanField(default=False)
+    name = models.CharField(max_length=1500)
+    qty = models.IntegerField(default=0)
+    price = models.IntegerField(default=0)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = "bread"
+        app_label = "api"
+
+    def __str__(self):
+        return self.name
+
+
+class Topping(models.Model):
+    id = models.AutoField(primary_key=True)
+    is_deleted = models.BooleanField(default=False)
+    name = models.CharField(max_length=1500)
+    qty = models.IntegerField(default=0)
+    price = models.IntegerField(default=0)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = "topping"
+        app_label = "api"
+
+    def __str__(self):
+        return self.name
+
+
+class Cheese(models.Model):
+    id = models.AutoField(primary_key=True)
+    is_deleted = models.BooleanField(default=False)
+    name = models.CharField(max_length=1500)
+    qty = models.IntegerField(default=0)
+    price = models.IntegerField(default=0)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = "cheese"
+        app_label = "api"
+
+    def __str__(self):
+        return self.name
+
+
+class Sauce(models.Model):
+    id = models.AutoField(primary_key=True)
+    is_deleted = models.BooleanField(default=False)
+    name = models.CharField(max_length=1500)
+    qty = models.IntegerField(default=0)
+    price = models.IntegerField(default=0)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = "sauce"
+        app_label = "api"
+
+    def __str__(self):
+        return self.name
+
+
+class Sandwich(models.Model):
+    id = models.AutoField(primary_key=True)
+    is_deleted = models.BooleanField(default=False)
+    bread = models.ForeignKey(Bread, on_delete=models.SET_NULL, null=True)
+    topping = models.ForeignKey(Topping, on_delete=models.SET_NULL, null=True)
+    cheese = models.ForeignKey(Cheese, on_delete=models.SET_NULL, null=True)
+    sauce = models.ForeignKey(Sauce, on_delete=models.SET_NULL, null=True)
+    created_date = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        db_table = "sandwich"
+        app_label = "api"
+
+    def __str__(self):
+        return self.id
