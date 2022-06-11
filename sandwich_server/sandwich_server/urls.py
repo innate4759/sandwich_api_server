23c0c3f28273ebfab2c6afb7fbaa741d48225412
박현규 <piga2000@builton.co.kr>
2022-06-12 06:10:14 +0900

    # api url 패턴 변경

--- sandwich_server/sandwich_server/urls.py
+++ sandwich_server/sandwich_server/urls.py
@@ -18,6 +18,11 @@ from django.urls import path, include, re_path
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
 
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path("api/", include("api.urls")),
+]
+
 schema_view = get_schema_view(
     openapi.Info(
         title="Sandwich API",
@@ -27,11 +32,6 @@ schema_view = get_schema_view(
     ),
     public=True,
 )
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path("api", include("api.urls")),
-]
 
 urlpatterns += [
         re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name="schema-json"),
