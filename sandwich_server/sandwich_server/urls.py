d62814975e0e2928314f11fe2f5f7b8ed0537acf
박현규 <piga2000@builton.co.kr>
2022-06-11 23:24:34 +0900

    # API 명세서 추가 drf-ysg 라이브러리 추가 /swagger   /redoc url 추가

--- sandwich_server/sandwich_server/urls.py
+++ sandwich_server/sandwich_server/urls.py
@@ -14,9 +14,26 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Sandwich API",
+        default_version="v1",
+        description="Sandwich API 명세서",
+        contact=openapi.Contact(name="박현규", email="piga2000@naver.com"),
+    ),
+    public=True,
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("api", include("api.urls")),
 ]
+
+urlpatterns += [
+        re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name="schema-json"),
+        re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+        re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),    ]
